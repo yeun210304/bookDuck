12954ec2191b2778902cb5c9b017aa17b2cf4483
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-06-08 16:44:55 +0900

    기초세팅 완료
    210608

---
+++ bookDuck/src/main/java/com/spring/bookduck/controller/BookDuckController.java
@@ -0,0 +1,16 @@
+package com.spring.bookduck.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookDuckController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String test() {
+		
+		
+		return "home";
+	}
+}

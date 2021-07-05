23b07482888ca78a40222378f88af96fd0c497e1
juiont <juiont@naver.com>
2021-07-05 13:36:07 +0900

    0705 커밋

--- bookDuck/src/main/java/com/spring/bookduck/controller/LoginController.java
+++ bookDuck/src/main/java/com/spring/bookduck/controller/LoginController.java
@@ -12,12 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.spring.bookduck.mail.MailDto;
-import com.spring.bookduck.mail.MailSender;
 import com.spring.bookduck.model.biz.LoginBiz;
 import com.spring.bookduck.model.dto.MemberDto;
 
